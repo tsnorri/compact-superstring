a9816d8cc48b9bebe628b0670f4fffb493eadae0
Jarno Niklas Alanko <alanko.jarno@gmail.com>
2016-10-10 15:40:12 +0300

    Superstring callback compiles

--- tribble/find-superstring/superstring_callback.cc
+++ tribble/find-superstring/superstring_callback.cc
@@ -16,9 +16,96 @@
 
 #include "superstring_callback.hh"
 #include <iostream>
+#include <cassert>
+#include <algorithm>
+
+Superstring_callback::Superstring_callback(std::size_t n_strings) 
+: merges_done(0), n_strings(n_strings), leftend(n_strings), next(n_strings), rightavailable(n_strings){
+    for(std::size_t i = 0; i < n_strings; i++){
+        leftend[i] = i;
+        next[i] = i+1;
+        rightavailable[i] = true;
+    }
+}
+
+bool Superstring_callback::try_merge(std::size_t left_string, std::size_t right_string, std::size_t overlap_length){
+    if(leftend[right_string] != left_string){
+        merges.push_back(std::make_tuple(left_string, right_string, overlap_length));
+        rightavailable[right_string] = false;
+        leftend[right_string] = leftend[left_string];
+        return true;
+    }
+    return false;
+}
+
+
 
 bool Superstring_callback::callback(std::size_t read_lex_rank, std::size_t match_length, std::size_t match_sa_begin, std::size_t match_sa_end){
-    std::cout << read_lex_rank << " " << match_length << " " << match_sa_begin << " " << match_sa_end << std::endl;
+    if(merges_done >= n_strings - 1) return true; // No more merges can be done
     
-    return true;
+    //std::cout << read_lex_rank << " " << match_length << " " << match_sa_begin << " " << match_sa_end << std::endl;
+    assert(read_lex_rank != 0);
+    read_lex_rank--; // Change to 0-based indexing
+    
+    std::size_t k = get_next_right_available(match_sa_begin-1);
+    if(k > match_sa_end) {
+        // Next one is outside of the suffix array interval, or not found at all
+        return false;
+    }
+    
+    if(try_merge(read_lex_rank, k, match_length)) return true;
+    
+    // Failed, try again a second time
+    k = get_next_right_available(k);
+    
+    if(k > match_sa_end) {
+        // Next one is outside of the suffix array interval, or not found at all
+        return false;
+    }
+    
+    if(try_merge(read_lex_rank, k, match_length)) return true;
+    
+    return false; // Should never come here, because the second try should always be succesful
+    
 }
+
+std::size_t Superstring_callback::get_next_right_available(std::size_t index){
+    for(std::size_t i = index+1; i < n_strings; i++){
+        if(rightavailable[i]) 
+            return i;
+    }
+    return n_strings;
+}
+
+std::string Superstring_callback::build_final_superstring(std::vector<std::string> strings){
+    if(strings.size() == 0) return "";
+    if(strings.size() == 1) return strings[0];
+    
+    std::sort(merges.begin(), merges.end());
+    std::size_t current_string_idx = n_strings;
+    
+    // Initilize current_string_idx to the first string in the final superstring
+    for(std::size_t i = 0; i < n_strings; i++){
+        if(rightavailable[i]){
+            current_string_idx = i;
+            break;
+        }
+    }
+    assert(current_string_idx != n_strings);
+        
+    // Concatenate all strings putting in the overlapping region of adjacent strings only once
+    std::size_t current_overlap_length;
+    std::string superstring;
+    for(std::size_t i = 0; i < n_strings - 1; i++){
+        std::size_t left_string_idx = std::get<0> (merges[current_string_idx]);
+        std::size_t right_string_idx = std::get<1> (merges[current_string_idx]);
+        std::size_t overlap = std::get<1> (merges[current_string_idx]);
+        
+        superstring += strings[left_string_idx].substr(strings[left_string_idx].size() - overlap);
+        if(i == n_strings - 2) superstring += strings[right_string_idx]; // Last iteration
+        left_string_idx = right_string_idx;
+    }
+    
+    return superstring;
+}
+
--- tribble/find-superstring/superstring_callback.hh
+++ tribble/find-superstring/superstring_callback.hh
@@ -19,17 +19,39 @@
 #define TRIBBLE_SUPERSTRING_CALLBACK_HH
 
 #include <cstddef> // std::size_t
+#include <vector>
+#include <string>
+#include <tuple>
 
 class Superstring_callback{
   
 public:
     
-    bool callback(std::size_t read_lex_rank, std::size_t match_length, std::size_t match_sa_begin, std::size_t match_sa_end);
+	Superstring_callback(std::size_t n_strings);
+	
+	// Returns true if merge was successful, or if no more merges can be done
+	bool callback(std::size_t read_lex_rank, std::size_t match_length, std::size_t match_sa_begin, std::size_t match_sa_end);
+	
+	std::string build_final_superstring(std::vector<std::string> strings); // Call after all prefix-suffix overlaps have been considered
     
 private:
-    
+	
+	// Returns n_strings if not found, else the index of the next one-bit in rightavailable to the right of index
+	// Parameter index can be zero. In that case returns the first one-bit in the entire rightavailable array.
+	std::size_t get_next_right_available(std::size_t index);
+	
+	// Merges two strings if the merge would not create a cycle
+	bool try_merge(std::size_t left_string, std::size_t right_string, std::size_t overlap_length);
+	
+	std::size_t merges_done;
+	
+	std::size_t n_strings;
+	// todo: use sdsl containers
+	std::vector<std::size_t> leftend;
+	std::vector<std::size_t> next;
+	std::vector<bool> rightavailable;
+	std::vector<std::tuple<std::size_t,std::size_t,std::size_t> > merges; // triples (left read, right read, overlap length)
+	
 };
-
-//bool superstring_callback(std::size_t read_lex_rank, std::size_t match_length, std::size_t match_sa_begin, std::size_t match_sa_end);
 
 #endif
