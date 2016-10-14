912b645fe2f9d00de705094decc175d222ad6602
Tuukka Norri <tsnorri@iki.fi>
2016-10-15 02:37:05 +0300

    Use sdsl::wt_blcd instead of wt_huff
    – The latter causes an assertion failure when using species_level.fna as input.

--- tribble/find-superstring/find_superstring.hh
+++ tribble/find-superstring/find_superstring.hh
@@ -33,7 +33,8 @@
 #endif
 
 
-typedef sdsl::wt_huff <>							wt_type;
+//typedef sdsl::wt_huff <>							wt_type;
+typedef sdsl::wt_blcd <>							wt_type;
 typedef sdsl::csa_wt <wt_type, 1 << 20, 1 << 20>	csa_type;
 typedef sdsl::lcp_support_tree2 <256>				lcp_support_type;
 typedef sdsl::cst_sct3 <csa_type, lcp_support_type>	cst_type;
