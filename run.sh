c349f0e726880c9e5ab88fb91488622a5fffe96a
Jarno Niklas Alanko <alanko.jarno@gmail.com>
2016-10-17 15:07:40 +0300

    Add shell script that builds the index and computes the superstring

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+name=$1
+
+if [ -z "$name" ]; 
+	then echo "Give input .fna input file (without .fna extension) as a command line argument";
+	exit
+fi
+
+# Clean up
+rm -f testcases/${name}.sdsl
+rm -f testcases/${basic}.sorted
+
+# Build index
+./tribble/find-superstring/find-superstring -C -f testcases/${name}.fna -i testcases/${name}.sdsl -s testcases/${name}.sorted
+
+# Compute superstring
+./tribble/find-superstring/find-superstring --find-superstring -i testcases/${name}.sdsl -s testcases/${name}.sorted
