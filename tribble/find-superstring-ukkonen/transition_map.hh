30cdb8d99411ea8626a73cba8bd1482547d58868
tsnorri <tsnorri@iki.fi>
2017-07-19 16:36:10 +0300

    Return iterator ranges to avoid copying

--- tribble/find-superstring-ukkonen/transition_map.hh
+++ tribble/find-superstring-ukkonen/transition_map.hh
@@ -18,8 +18,8 @@
 #ifndef TRIBBLE_TRANSITION_MAP_HH
 #define TRIBBLE_TRANSITION_MAP_HH
 
-#include <boost/iterator/iterator_adaptor.hpp>
-
+#include <boost/iterator/transform_iterator.hpp>
+#include <boost/range/iterator_range.hpp>
 
 namespace tribble {
 	
@@ -37,15 +37,36 @@ namespace tribble {
 	class transition_map : public transition_map_base
 	{
 	public:
-		typedef typename t_state_ptr::pointer			ptr_type;
-		typedef std::vector <t_state_ptr>				vector_type;
-		typedef typename vector_type::size_type			size_type;
-		typedef typename vector_type::reference			reference;
-		typedef typename vector_type::const_reference	const_reference;
+		typedef typename t_state_ptr::pointer							ptr_type;
+		typedef std::vector <t_state_ptr>								state_vector_type;
+		typedef std::vector <uint8_t>									used_state_vector_type;
+		typedef typename state_vector_type::size_type					size_type;
+		typedef typename state_vector_type::reference					reference;
+		typedef typename state_vector_type::const_reference				const_reference;
+		typedef typename used_state_vector_type::const_iterator			const_transition_iterator;
 		
 	protected:
-		vector_type					m_states{};
-		std::vector <uint8_t>		m_used_states{};
+		// Functor for accessing the numbered state.
+		class access_state_ptr
+		{
+		protected:
+			state_vector_type const *m_states{nullptr};
+			
+		public:
+			access_state_ptr(state_vector_type const &states):
+				m_states(&states)
+			{
+			}
+			
+			ptr_type operator()(std::size_t const idx) const { return (*m_states)[idx].get(); }
+		};
+		
+	public:
+		typedef boost::transform_iterator <access_state_ptr, used_state_vector_type::const_iterator>	const_state_ptr_iterator;
+		
+	protected:
+		state_vector_type		m_states{};
+		used_state_vector_type	m_used_states{};
 		
 	public:
 		transition_map():
@@ -74,18 +95,19 @@ namespace tribble {
 			return false;
 		}
 		
-		template <typename T>
-		void get_states(T &result) const
+		boost::iterator_range <const_state_ptr_iterator>
+		get_states() const
 		{
-			for (auto const idx : m_used_states)
-				result.push_back(m_states[idx].get());
+			access_state_ptr fn(m_states);
+			auto const begin(const_state_ptr_iterator(m_used_states.cbegin(), fn));
+			auto const end(const_state_ptr_iterator(m_used_states.cend(), fn));
+			return boost::make_iterator_range(begin, end);
 		}
 		
-		template <typename T>
-		void get_transitions(T &result) const
+		boost::iterator_range <const_transition_iterator>
+		get_transitions() const
 		{
-			for (auto const idx : m_used_states)
-				result.push_back(idx);
+			return boost::make_iterator_range(m_used_states.cbegin(), m_used_states.cend());
 		}
 	};
 }
